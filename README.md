89793328388c9709f0434dae74acb93b699d3e19
Logan <logannbjork1@gmail.com>
2026-01-02 00:01:57 -0500

    changed UI

---
+++ README.md
@@ -0,0 +1,1 @@
+# simple gain test plugin before doing anything crazy in YAWP
---
+++ src/PluginEditor.cpp
@@ -0,0 +1,52 @@
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
+    : AudioProcessorEditor (&p), processorRef (p)
+{
+    juce::ignoreUnused (processorRef);
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (200, 400);
+
+	gainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+	gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 100, 25);
+	gainSlider.setRange(-48.0, 0.0);
+	gainSlider.setValue(-1.0);
+	gainSlider.addListener(this); // LISTENER: Actually makes it work!
+	addAndMakeVisible(gainSlider);
+}
+
+AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    g.fillAll (juce::Colour(26, 26, 30));
+	
+	// draw slider
+	gainSlider.setBounds(getLocalBounds());
+	gainSlider.getLookAndFeel().setColour(juce::Slider::backgroundColourId, juce::Colour(134, 68, 73));
+	gainSlider.getLookAndFeel().setColour(juce::Slider::trackColourId, juce::Colour(214, 222, 223));
+	gainSlider.getLookAndFeel().setColour(juce::Slider::thumbColourId, juce::Colour(97, 101, 119));
+	
+    // g.setColour (juce::Colour(214, 222, 223));
+    // g.setFont (15.0f);
+    // g.drawFittedText ("Hello!", getLocalBounds(), juce::Justification::centred, 1);
+}	
+
+void AudioPluginAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
+
+void AudioPluginAudioProcessorEditor::sliderValueChanged (juce::Slider* slider) {
+	if(slider == &gainSlider) {
+		// calculate decibels
+		processorRef.rawVolume = pow(10, gainSlider.getValue() / 20); // div by 20 
+	}
+}
