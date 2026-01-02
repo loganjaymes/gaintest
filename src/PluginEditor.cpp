5d66c2e85faa082d7efbf0073bb1facc363eb704
Logan <logannbjork1@gmail.com>
2026-01-02 04:12:01 -0500

    Dynamic sizing

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -3,12 +3,9 @@
 
 //==============================================================================
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
-    : AudioProcessorEditor (&p), processorRef (p)
-{
-    juce::ignoreUnused (processorRef);
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (200, 400);
+	: AudioProcessorEditor (&p), processorRef (p) {
+	juce::ignoreUnused (processorRef);
+	setSize(800, 450);
 
 	gainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
 	gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 100, 25);
@@ -18,35 +15,35 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
 	addAndMakeVisible(gainSlider);
 }
 
-AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
-{
+AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor() {
 }
 
 //==============================================================================
-void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    g.fillAll (juce::Colour(26, 26, 30));
+void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g) {
+	g.fillAll (juce::Colour(26, 26, 30));
 	
 	// draw slider
-	gainSlider.setBounds(getLocalBounds());
+	gainSlider.getLookAndFeel().setColour(juce::Slider::textBoxOutlineColourId, juce::Colour(190, 81, 77));
 	gainSlider.getLookAndFeel().setColour(juce::Slider::backgroundColourId, juce::Colour(134, 68, 73));
 	gainSlider.getLookAndFeel().setColour(juce::Slider::trackColourId, juce::Colour(214, 222, 223));
 	gainSlider.getLookAndFeel().setColour(juce::Slider::thumbColourId, juce::Colour(97, 101, 119));
 	
-    // g.setColour (juce::Colour(214, 222, 223));
-    // g.setFont (15.0f);
-    // g.drawFittedText ("Hello!", getLocalBounds(), juce::Justification::centred, 1);
+	// g.setColour (juce::Colour(214, 222, 223));
+	// g.setFont (15.0f);
+	// g.drawFittedText ("Hello!", getLocalBounds(), juce::Justification::centred, 1);
 }	
 
-void AudioPluginAudioProcessorEditor::resized()
-{
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+void AudioPluginAudioProcessorEditor::resized() {
+	// This is generally where you'll want to lay out the positions of any
+	// subcomponents in your editor..
+	
+	gainSlider.setBounds(0, 50, getWidth() / 3, getHeight() - 100);
+	//gainSlider.setBounds(0, 50, 250, 350);
 }
 
 void AudioPluginAudioProcessorEditor::sliderValueChanged (juce::Slider* slider) {
 	if(slider == &gainSlider) {
-		// calculate decibels
+		// calculate decibels and display
 		processorRef.rawVolume = pow(10, gainSlider.getValue() / 20); // div by 20 
 	}
 }
--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -6,20 +6,21 @@
 class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor, public juce::Slider::Listener
 {
 public:
-    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
-    ~AudioPluginAudioProcessorEditor() override;
+	explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+	~AudioPluginAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	//==============================================================================
+	void paint (juce::Graphics&) override;
+	void resized() override;
 	void sliderValueChanged(juce::Slider* slider) override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    AudioPluginAudioProcessor& processorRef;
+	// This reference is provided as a quick way for your editor to
+	// access the processor object that created it.
+	AudioPluginAudioProcessor& processorRef;
 
 	juce::Slider gainSlider;
+	// put distortion below hererarearereeee
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
